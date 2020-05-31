8197c417b066dd6cee531bdc7193e4db32878ff9
Jacob Paisley <jacobpaisley97@gmail.com>
2020-05-31 18:53:24 -0500

    Adding Nextjs, Blueprintjs

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "normalize.css"
+import "@blueprintjs/core/lib/css/blueprint.css"
+import "@blueprintjs/icons/lib/css/blueprint-icons.css"
+
+const App =({ Component, pageProps }) => {
+    return <Component {...pageProps} />
+}
+  
+export default App
