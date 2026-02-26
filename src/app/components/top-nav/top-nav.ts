9bd01bf5ba3b64eea67e5d12a86cff1cd6b098ce
Jacob Paisley <jacobpaisley97@gmail.com>
2026-02-26 10:42:13 -0800

    Adding login button in place of user profile icon if not logged in (topnav)

--- src/app/components/top-nav/top-nav.ts
+++ src/app/components/top-nav/top-nav.ts
@@ -1,13 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UserIcon } from '../user-icon/user-icon';
+import { UserService } from '../../services/user-service/user-service';
+import { Button } from '../button/button';
 
 @Component({
   selector: 'jwpaisley-top-nav',
-  imports: [UserIcon, RouterModule],
+  imports: [UserIcon, RouterModule, Button],
   templateUrl: './top-nav.html',
   styleUrl: './top-nav.scss',
 })
-export class TopNav {
+export class TopNav implements OnInit {
   @Input() showUserIcon = true;
+
+  protected isUserLoggedIn = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.isUserLoggedIn = this.userService.isUserLoggedIn();
+  }
 }
