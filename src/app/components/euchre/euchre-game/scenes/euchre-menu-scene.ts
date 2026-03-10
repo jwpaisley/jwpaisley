61bbbe2fb5f28d80da7157e0b4b8bbff14f6beaa
Jacob Paisley <jacobpaisley97@gmail.com>
2026-03-09 17:40:07 -0700

    Cleaning up euchre menu scene

--- src/app/components/euchre/euchre-game/scenes/euchre-menu-scene.ts
+++ src/app/components/euchre/euchre-game/scenes/euchre-menu-scene.ts
@@ -1,9 +1,52 @@
 import { CardUtility, CardData } from "../utils/card-util";
 
+const JACKS = [
+    { rank: 'J', suit: 'clubs' },
+    { rank: 'J', suit: 'hearts' },
+    { rank: 'J', suit: 'spades' },
+    { rank: 'J', suit: 'diamonds' },
+];
+
+const TITLE_FADE_IN_DURATION = 500;
+const TITLE_FADE_IN_DELAY = 3000;
+const TITLE_Y_MARGIN = -250;
+const MENU_BUTTON_FADE_IN_DURATION = 500;
+const MENU_BUTTON_FADE_IN_DELAY = 3500;
+
+const CARD_X_MARGIN = 100;
+const CARD_Y_MARGIN = 125;
+
 export class EuchreMenuScene extends Phaser.Scene {
-  constructor() {
-    super('EuchreMenuScene');
-  }
+    cardSprites: Phaser.GameObjects.Sprite[] = [];
+    title?: Phaser.GameObjects.Text = undefined;
+    newGameButton?: Phaser.GameObjects.Text = undefined;
+    joinGameButton?: Phaser.GameObjects.Text = undefined;
+    tutorialButton?: Phaser.GameObjects.Text = undefined;
+    settingsButton?: Phaser.GameObjects.Text = undefined;
+
+    constructor() {
+        super('EuchreMenuScene');
+    }
+
+    get centerX(): number {
+        return this.scale.width / 2;
+    }
+
+    get centerY(): number {
+        return this.scale.height / 2;
+    }
+
+    get titleY(): number {
+        return this.centerY + TITLE_Y_MARGIN;
+    }
+
+    get cardStartX(): number {
+        return this.scale.width / 2 - 150;
+    }
+
+    get cardY(): number {
+        return this.titleY + CARD_Y_MARGIN;
+    }
 
     preload() {
         this.load.image('card-back', 'https://storage.googleapis.com/jwpaisley-game-assets/euchre/standard-card-back.png');
@@ -14,85 +57,93 @@ export class EuchreMenuScene extends Phaser.Scene {
     }
 
     create() {
-        const { width, height } = this.scale;
-        const centerY = height / 2;
-        const startX = width / 2 - 150;
-        const titleY = centerY - 250;
-        const cardY = titleY + 125;
+        this.addJacksToMenuScene();
+        this.createFlipSequence();
+        this.addGameTitle();
+        this.addMenuButtons();
+        this.fadeInTitle();
+        this.fadeInMenuButtons();
+    }
 
-        // 1. Define the Jacks we want to reveal (Left to Right)
-        const jacks = [
-            { rank: 'J', suit: 'clubs' },
-            { rank: 'J', suit: 'hearts' },
-            { rank: 'J', suit: 'spades' },
-            { rank: 'J', suit: 'diamonds' },
-        ];
-
-        const cardSprites: Phaser.GameObjects.Sprite[] = [];
-
-        // 2. Render the four face-down cards
-        jacks.forEach((jackData, index) => {
-            const xPos = startX + (index * 100); // Space them out
-            const card = this.add.sprite(xPos, cardY, 'card-back');
-            card.setScale(1.2); // Make them a bit larger for the menu
-            
-            // Store the frame index we will flip to
-            card.setData('cardFace', CardUtility.getFrame(jackData));
-            cardSprites.push(card);
-        });
-
-        // 3. Create the sequential flip animation
-        this.createFlipSequence(cardSprites);
-
-        // 4. Create the 'EUCHRE' title (initially invisible)
-        const title = this.add.text(width / 2, centerY - 250, 'EUCHRE', { 
+    addGameTitle() {
+        this.title = this.add.text(this.scale.width / 2, this.titleY, 'EUCHRE', { 
             fontSize: '64px',
             color: '#fff',
             fontStyle: 'bold'
         })
         .setOrigin(0.5)
         .setAlpha(0);
-        
-        const newGameButton = this.add.text(width / 2, height / 2, 'NEW GAME', { 
-        fontSize: '32px', color: '#fff', fontStyle: 'bold'
+    }
+
+    fadeInTitle() {
+        this.tweens.add({
+            targets: this.title,
+            alpha: 1,
+            duration: TITLE_FADE_IN_DURATION,
+            delay: TITLE_FADE_IN_DELAY,
+            ease: 'Power2'
+        });
+    }
+
+    addMenuButtons() {
+        this.newGameButton = this.add.text(this.scale.width / 2, this.scale.height / 2, 'NEW GAME', { 
+            fontSize: '32px', color: '#fff', fontStyle: 'bold'
         })
         .setOrigin(0.5)
         .setInteractive({ useHandCursor: true })
         .on('pointerdown', () => this.createNewGame())
         .setAlpha(0);
 
-        const joinGameButton = this.add.text(width / 2, height / 2 + 50, 'JOIN GAME', { 
-        fontSize: '24px', color: '#fff', fontStyle: 'bold'
+        this.joinGameButton = this.add.text(this.scale.width / 2, this.scale.height / 2 + 50, 'JOIN GAME', { 
+            fontSize: '24px', color: '#fff', fontStyle: 'bold'
         })
         .setOrigin(0.5)
         .setInteractive({ useHandCursor: true })
-        .on('pointerdown', () => this.scene.launch('SettingsScene'))
+        .on('pointerdown', () => {})
         .setAlpha(0);
 
-        const settingsButton = this.add.text(width / 2, height / 2 + 100, 'SETTINGS', { 
-        fontSize: '24px', color: '#fff', fontStyle: 'bold'
+        this.tutorialButton = this.add.text(this.scale.width / 2, this.scale.height / 2 + 100, 'TUTORIAL', { 
+            fontSize: '24px', color: '#fff', fontStyle: 'bold'
         })
         .setOrigin(0.5)
         .setInteractive({ useHandCursor: true })
-        .on('pointerdown', () => this.scene.launch('SettingsScene'))
+        .on('pointerdown', () => {})
         .setAlpha(0);
 
-        // 5. Fade in the title after the cards are revealed
+        this.settingsButton = this.add.text(this.scale.width / 2, this.scale.height / 2 + 150, 'SETTINGS', { 
+            fontSize: '24px', color: '#fff', fontStyle: 'bold'
+        })
+        .setOrigin(0.5)
+        .setInteractive({ useHandCursor: true })
+        .on('pointerdown', () => {})
+        .setAlpha(0);
+    }
+
+    fadeInMenuButtons() {
         this.tweens.add({
-            targets: title,
+            targets: [
+                this.newGameButton, 
+                this.joinGameButton,
+                this.tutorialButton, 
+                this.settingsButton
+            ],
             alpha: 1,
-            duration: 500,
-            delay: 3500, // Wait for the flip sequence to finish
+            duration: MENU_BUTTON_FADE_IN_DURATION,
+            delay: MENU_BUTTON_FADE_IN_DELAY,
             ease: 'Power2'
         });
+    }
 
-        // 5. Fade in the title after the cards are revealed
-        this.tweens.add({
-            targets: [newGameButton, joinGameButton, settingsButton],
-            alpha: 1,
-            duration: 500,
-            delay: 4000, // Wait for the flip sequence to finish
-            ease: 'Power2'
+    addJacksToMenuScene() {
+        this.cardSprites = [];
+
+        JACKS.forEach((jackData, index) => {
+            const xPos = this.cardStartX + (index * CARD_X_MARGIN);
+            const card = this.add.sprite(xPos, this.cardY, 'card-back');
+            card.setScale(1.2);
+            card.setData('cardFace', CardUtility.getFrame(jackData));
+
+            this.cardSprites.push(card);
         });
     }
 
@@ -101,27 +152,26 @@ export class EuchreMenuScene extends Phaser.Scene {
         this.scene.start('EuchreGameScene', { gameId: newId });
     }
 
-    private createFlipSequence(cards: Phaser.GameObjects.Sprite[]) {
-        cards.forEach((card, index) => {
+    private createFlipSequence() {
+        this.cardSprites.forEach((card, index) => {
             const timeline = this.add.timeline([
                 {
-                    at: index * 500, // Stagger the start of each card flip
+                    at: index * 500,
                     tween: {
                         targets: card,
-                        scaleX: 0, // Shrink to a "line"
+                        scaleX: 0,
                         duration: 250,
                         ease: 'Linear',
                         onComplete: () => {
-                            // Swap texture to the front face (using our helper frame)
                             card.setTexture('cards', card.getData('cardFace'));
                         }
                     }
                 },
                 {
-                    at: (index * 500) + 250, // Start expanding immediately after shrink
+                    at: (index * 500) + 250,
                     tween: {
                         targets: card,
-                        scaleX: 1.2, // Expand back to full width
+                        scaleX: 1.2,
                         duration: 250,
                         ease: 'Linear'
                     }
